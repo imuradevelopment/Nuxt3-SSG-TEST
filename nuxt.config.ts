94b73535f0ee59b46e666157b61802f83aaa3cb5
imuradevelopment <imuradevelopmentauth@gmail.com>
2023-09-24 16:57:25 +0900

    cdnURLを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        cdnURL: '/Nuxt3-SSG-TEST'
+    },
     devtools: { enabled: true },
     modules: [
         '@element-plus/nuxt'
