8de9cdbad4b82ee6b01ad926e813894ff3825d51
imuradevelopment <imuradevelopmentauth@gmail.com>
2023-09-24 19:49:39 +0900

    cdnURLを削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    // ssr: true,
-    app: {
-        cdnURL: '/Nuxt3-SSG-TEST'
-    },
+    ssr: true,
+    // app: {
+    //     cdnURL: '/Nuxt3-SSG-TEST'
+    // },
     nitro: {
         // preset: 'node-server'
         // preset: 'netlify'
         // preset: 'netlify_edge'
-        preset: 'netlify_builder'
+        // preset: 'netlify_builder'
     },
     devtools: { enabled: true },
     modules: [
         '@element-plus/nuxt'
     ],
+    elementPlus: { /** Options */ }
 })
