964c9754e7fcf2682e05b1bfd68401e6286188f4
imuradevelopment <imuradevelopmentauth@gmail.com>
2023-09-24 21:05:32 +0900

    baseURL, cdnURLの更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
     ssr: true,
     app: {
         baseURL: '/Nuxt3-SSG-TEST',
+        cdnURL: '/Nuxt3-SSG-TEST',
     },
     nitro: {
         // preset: 'node-server'
