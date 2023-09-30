0e2632369ceb93b86c78a663ca062338e2cf5cbb
imuradevelopment <imuradevelopmentauth@gmail.com>
2023-09-30 17:11:28 +0900

    elementPlusの設定値を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,26 @@ export default defineNuxtConfig({
         '@element-plus/nuxt',
         'nuxt-microcms-module'
     ],
-    elementPlus: { /** Options */ },
+    elementPlus: {
+        importStyle: 'css',
+        themes: ['dark'],
+        icon: 'ElIcon',
+        namespace: 'el',
+        injectionID: { prefix: 1024, current: 0 },
+        // ElTooltipコンポーネントをベースとするすべてのappend-to propsを修正する場合、ここに値を追加する必要があります。
+        // appendTo: [],
+        // Element Plusから自動的にインポートされないコンポーネントがある場合は、ここにコンポーネント名を追加する必要があります。
+        // components: ['ElSubMenu'],
+        // サブコンポーネントの定義ファイルが親コンポーネントにあるコンポーネントのマップ。
+        // subComponents: {},
+        // Element Plusから自動的にインポートされないディレクティブがある場合は、ここにディレクティブ名を追加する必要があります。
+        // directives: {},
+        // Element Plusからコンテンツを自動的にインポートしたい場合は、ここに追加できます。
+        // imports: [],
+        // コンポーネントが正しくスタイルをロードしない場合、ここにコンポーネント名を追加する必要があります。
+        // noStylesComponents: [],
+        
+    },
     microCMS: {
         serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
         apiKey: process.env.MICROCMS_API_KEY,
