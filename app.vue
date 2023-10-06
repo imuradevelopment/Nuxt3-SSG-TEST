d999febc84938c134aa4322fa944f5fbb9adf257
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2023-10-06 16:14:05 +0900

    100vhをbodyに適応(IOS対応)

--- app.vue
+++ app.vue
@@ -17,17 +17,22 @@ html {
 }
 
 body {
-    min-height: 100vh;
     min-height: -webkit-fill-available;
-}
-
-.layoutThreeLayers {
     display: grid;
     grid-template-rows: auto 1fr auto;
-}
-
-.layoutSinglePage {
+    min-height: 100vh;
     max-width: 1980px;
     margin: 0 auto;
 }
+
+/* .layoutThreeLayers {
+    display: grid;
+    grid-template-rows: auto 1fr auto;
+    min-height: 100vh;
+} */
+
+/* .layoutSinglePage {
+    max-width: 1980px;
+    margin: 0 auto;
+} */
 </style>
