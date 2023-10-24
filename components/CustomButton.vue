d24681eaff58640349617282f5f29c12e6112d6a
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2023-10-24 17:31:45 +0900

    カスタムボタンのsvg追加

--- components/CustomButton.vue
+++ components/CustomButton.vue
@@ -7,9 +7,26 @@
                     MORE
                 </slot>
             </span>
-            <el-icon size="1.5rem" style="margin-bottom: 0.1rem;" class="mr-2 rounded-full" :class="hoverArrowType">
+            <!-- <el-icon size="1.5rem" style="margin-bottom: 0.1rem;" class="mr-2 rounded-full" :class="hoverArrowType">
                 <CaretRight />
-            </el-icon>
+            </el-icon> -->
+            <span class="h-6 w-9">
+                <!-- <svg height="24" width="36" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="19" cy="12" r="12" stroke="white" stroke-width="3" fill="none"/>
+                <line x1="0" y1="12" x2="22" y2="12" stroke="white" stroke-width="3"/>
+                <polygon points="28,8 28,16 32,12" fill="white"/>
+                </svg> -->
+                <!-- <svg height="30" width="38" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="24" cy="12" r="12" stroke="white" stroke-width="3" fill="none"/>
+                <line x1="0" y1="12" x2="32" y2="12" stroke="white" stroke-width="3"/>
+                <polygon points="28,9 28,15 32,12" fill="white"/>
+                </svg> -->
+                <svg height="24" width="36" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="19" cy="12" r="12" stroke="white" stroke-width="3" fill="none"/>
+                <line x1="0" y1="12" x2="22" y2="12" stroke="white" stroke-width="3"/>
+                <polygon points="22,9 22,15 26,12" fill="white"/>
+                </svg>
+            </span>
     </NuxtLink>
 </template>
   
@@ -56,4 +73,20 @@ const buttonStyle = [
   props.buttonStyle === 'square' ? [ ''] : '',
 ];
 </script>
-<style scoped></style>  
+<style scoped>
+/* 背景色を青に変更 */
+.background {
+  fill: blue;
+}
+
+/* 丸の塗りつぶし色を白に変更 */
+.circle {
+  fill: white;
+}
+
+/* 矢印の塗りつぶし色と線の色を黄色に変更 */
+.arrow-head, .arrow-tail {
+  fill: yellow;
+  stroke: yellow;
+}
+</style>  
