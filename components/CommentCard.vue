8e169700aa1dd163cad1c21226a1983e5989bebe
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2023-12-05 20:54:12 +0900

    事業情報ページの作成

--- components/CommentCard.vue
+++ components/CommentCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="p-4 mx-auto">
         <div class="bg-white p-4 rounded-lg relative box-border border border-collapse border-custom-gray">
-            <div class="absolute box-content border-collapse -left-[9.5px] top-5 bg-white border-l border-b border-custom-gray transform rotate-45 w-4 h-4"></div>
+            <div class="absolute box-content border-collapse -left-[9px] top-5 bg-white border-l border-b border-custom-gray transform rotate-45 w-4 h-4"></div>
             <slot name="content">
 
             </slot>
