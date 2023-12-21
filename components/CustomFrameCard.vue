9d0e8ce1d189b7aa576b9c65b52a217badb904c8
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2023-12-21 17:57:09 +0900

    事業情報ページの作成

--- components/CustomFrameCard.vue
+++ components/CustomFrameCard.vue
@@ -1,11 +1,62 @@
 <template>
-    <div class="flex items-center justify-center flex-col">
+    <div class="wrapper">
         <h3
-            class="ml-4 md:ml-0 py-2 px-4 text-base md:text-lg font-bold break-keep text-center text-custom-gray rounded border border-custom-gray w-fit translate-y-1/2 bg-white">
+            class="title">
             <slot name="title"></slot>
         </h3>
-        <p class="max-w-3xl w-full p-4 pt-8 text-sm md:text-base leading-7 md:leading-8 break-keep text-center rounded border border-custom-gray">
+        <p
+            class="content">
             <slot name="content"></slot>
         </p>
     </div>
 </template>
+<style scoped>
+.wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+}
+
+.title {
+    padding-top: 0.5rem;
+    padding-bottom: 0.5rem;
+    padding-left: 1rem;
+    padding-right: 1rem;
+    font-size: 1rem;
+    font-weight: bold;
+    overflow-wrap: break-word;
+    text-align: center;
+    color: var(--custom-color-gray);
+    border-radius: 0.25rem;
+    border: 1px solid var(--custom-color-gray);
+    width: max-content;
+    transform: translateY(50%);
+    background-color: white;
+
+    @media (min-width: 768px) {
+        font-size: 1.125rem;
+    }
+
+
+}
+
+.content {
+    max-width: 48rem;
+    width: 100%;
+    padding: 1rem;
+    padding-top: 2rem;
+    line-height: 1.75rem;
+    font-size: 0.875rem;
+    white-space: pre-line;
+    overflow-wrap: break-word;
+    text-align: center;
+    border-radius: 0.25rem;
+    border: 1px solid var(--custom-color-gray);
+
+    @media (min-width: 768px) {
+        line-height: 2rem;
+        font-size: 1rem;
+    }
+}
+</style>
