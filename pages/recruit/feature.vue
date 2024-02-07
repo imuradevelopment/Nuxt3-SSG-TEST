8e310be0701bf5ed78eaf89f2c8ee9fa83cfdcad
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-02-07 15:09:51 +0900

    特徴ページの作成

--- pages/recruit/feature.vue
+++ pages/recruit/feature.vue
@@ -97,7 +97,9 @@
                                     プログラミング未経験者が多いSier会社ではスキル不足により単純作業しか任せてもらえないことが多いです。ベイルでは文系・IT未経験でも確実にエンジニアとして成長できる環境を用意しており、入社後の1年間様々な研修・OJTで、技術者としての基礎を築きます。
                                 </div>
                                 <div class="self-end">
-                                    <CustomArrowButton arrowType="transition" to="/recruit/induction">研修内容を見る</CustomArrowButton>
+                                    <CustomArrowButton  arrowType="transition" to="/recruit/induction">
+                                        <template>研修内容を見る</template>
+                                    </CustomArrowButton>
                                 </div>
                         </div>
                     </div>
