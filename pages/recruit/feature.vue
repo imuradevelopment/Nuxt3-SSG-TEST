cf8b44116a00786ee84c1479d2c16927fb013143
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-02-07 15:02:14 +0900

    特徴ページの作成

--- pages/recruit/feature.vue
+++ pages/recruit/feature.vue
@@ -11,8 +11,8 @@
             </div>
         </section>
 
-        <section>
-            <div class="mt-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:space-x-4">
+        <!-- <section>
+            <div class="mt-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:gap-x-4">
                 <div class="order-2 max-w-screen mb-4 sm:mb-0 sm:order-1 col-span-1 row-span-2 overflow-hidden">
                     <div class="z-10 relative w-[95%] h-[95%] bg-gray-200"></div>
                     <img class="z-20 relative bottom-[90%] left-[5%] w-[95%] h-[95%] object-cover shadow-md"
@@ -45,17 +45,95 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
 
         <section>
-            <div class="mt-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:space-x-4">
+            <div class="mt-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-3 sm:grid-flow-col sm:gap-x-4">
+                <div class="order-2 sm:order-1 max-w-screen mb-4 sm:mb-0 col-span-1 sm:row-span-3 overflow-hidden">
+                    <div class="z-10 relative w-[95%] h-[95%] bg-gray-200"></div>
+                    <img class="z-20 relative bottom-[90%] left-[5%] w-[95%] h-[95%] object-cover shadow-md"
+                        src="/images/recruit/feature/002.png" />
+                </div>
+                <div class="order-1 sm:order-2 col-span-1 sm:row-span-1">
+                    <div class="mb-4 sm:mb-0 text-2xl font-bold">Point01</div>
+                    <div class="mb-4 sm:mb-0">
+                        <div class="mb-1 px-4 w-fit text-white bg-custom-deepBlue text-xl font-bold leading-loose">
+                            日本アイビーエムの
+                        </div>
+                        <div class="mb-1 px-4 w-fit text-white bg-custom-deepBlue text-xl font-bold leading-loose">
+                            コアパートナー
+                        </div>
+                    </div>
+                </div>
+                <div class="order-3 mb-4 sm:mb-0 col-span-1 sm:row-span-2">
+                    <div class="p-4 bg-gray-200 text-xs sm:text-base font-bold">
+                        世界のリーディングカンパニーであるアイビーエム社は最新のプロダクトやノウハウを駆使したシステムを得意としています。そのようなシステム構築に、提案段階から共同で作業することもあります。また、コベルコシステム株式会社、三井情報株式会社のコアパートナーであり、最先端の技術やノウハウの習得に大きなメリットがあります。
+                    </div>
+                </div>
+            </div>
+        </section>
+
+            <section>
+                <div class="mt-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-3 sm:grid-flow-col sm:gap-x-4">
+                    <div class="order-2 sm:order-3 max-w-screen mb-4 sm:mb-0 col-span-1 sm:row-span-3 overflow-hidden">
+                        <div class="z-10 relative w-[95%] h-[95%] bg-gray-200"></div>
+                        <img class="z-20 relative bottom-[90%] left-[5%] w-[95%] h-[95%] object-cover shadow-md"
+                            src="/images/recruit/feature/003.png" />
+                    </div>
+                    <div class="order-1 sm:order-1 col-span-1 sm:row-span-1">
+                        <div class="mb-4 sm:mb-0 text-2xl font-bold">Point02</div>
+                        <div class="mb-4 sm:mb-0">
+                            <div class="mb-1 px-4 w-fit text-white bg-custom-deepBlue text-xl font-bold leading-loose">
+                                未経験でも安心
+                            </div>
+                            <div class="mb-1 px-4 w-fit text-white bg-custom-deepBlue text-xl font-bold leading-loose">
+                                充実した研修制度
+                            </div>
+                        </div>
+                    </div>
+                    <div class="order-3 sm:order-2 mb-4 sm:mb-0 col-span-1 sm:row-span-2">
+                        <div class="p-4 bg-gray-200 text-xs sm:text-base font-bold">
+                            プログラミング未経験者が多いSier会社ではスキル不足により単純作業しか任せてもらえないことが多いです。ベイルでは文系・IT未経験でも確実にエンジニアとして成長できる環境を用意しており、入社後の1年間様々な研修・OJTで、技術者としての基礎を築きます。
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section>
+                <div class="my-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:grid-rows-3 sm:grid-flow-col sm:gap-x-4">
+                    <div class="order-2 sm:order-1 max-w-screen mb-4 sm:mb-0 col-span-1 sm:row-span-3 overflow-hidden">
+                        <div class="z-10 relative w-[95%] h-[95%] bg-gray-200"></div>
+                        <img class="z-20 relative bottom-[90%] left-[5%] w-[95%] h-[95%] object-cover shadow-md"
+                            src="/images/recruit/feature/004.png" />
+                    </div>
+                    <div class="order-1 sm:order-2 col-span-1 sm:row-span-1">
+                        <div class="mb-4 sm:mb-0 text-2xl font-bold">Point03</div>
+                        <div class="mb-4 sm:mb-0">
+                            <div class="mb-1 px-4 w-fit text-white bg-custom-deepBlue text-xl font-bold leading-loose">
+                                社員の育成を第一に
+                            </div>
+                            <div class="mb-1 px-4 w-fit text-white bg-custom-deepBlue text-xl font-bold leading-loose">
+                                考える
+                            </div>
+                        </div>
+                    </div>
+                    <div class="order-3 mb-4 sm:mb-0 col-span-1 sm:row-span-2">
+                        <div class="p-4 bg-gray-200 text-xs sm:text-base font-bold">
+                            売上目標や利益目標を設けず、新しい技術、得たい技術などが学べる現場は、利益度外視で受ける、年齢や経験を重視せず先輩が居ても若手社員がリーダーを任せる、組織を細分化せず様々な業務を経験する事で技術の相乗的成長を図るなどベイルは社員の育成を第一に考えています。
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        <!-- <section>
+            <div class="mt-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:gap-x-4">
                 <div class="order-2 max-w-screen mb-4 sm:mb-0 col-span-1 row-span-2 overflow-hidden">
                     <div class="z-10 relative w-[95%] h-[95%] bg-gray-200"></div>
                     <img class="z-20 relative bottom-[90%] left-[5%] w-[95%] h-[95%] object-cover shadow-md"
                         src="/images/recruit/feature/003.png" />
                 </div>
                 <div class="order-1 col-span-1 row-span-1">
-                    <div class="mb-4 sm:mb-0 text-2xl font-bold">Point01</div>
+                    <div class="mb-4 sm:mb-0 text-2xl font-bold">Point02</div>
                     <div class="mb-4 sm:mb-0">
                         <div class="hidden sm:inline-block">
                             <div class="mb-1 px-4 w-fit text-white bg-custom-deepBlue text-xl font-bold leading-loose">
@@ -88,10 +166,10 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
 
-        <section>
-            <div class="mt-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:space-x-4">
+        <!-- <section>
+            <div class="mt-12 px-4 sm:mx-0 grid grid-cols-1 sm:grid-cols-2 sm:gap-x-4">
                 <div class="order-2 max-w-screen mb-4 sm:mb-0 sm:order-1 col-span-1 row-span-2 overflow-hidden">
                     <div class="z-10 relative w-[95%] h-[95%] bg-gray-200"></div>
                     <img class="z-20 relative bottom-[90%] left-[5%] w-[95%] h-[95%] object-cover shadow-md"
@@ -124,7 +202,8 @@
                     </div>
                 </div>
             </div>
-        </section>
+        </section> -->
+
     </div>
 </template>
 
