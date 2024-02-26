b64132597ff3c63ad9029d55671406aa29ab47ce
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-02-26 16:21:49 +0900

    透過カードの追加

--- pages/recruit/induction.vue
+++ pages/recruit/induction.vue
@@ -55,22 +55,22 @@
                 キャリアパス</h2>
             <div class="mx-4">
                 <div class="flex flex-col justify-center items-center">
-                    <BlurGlassCard :blur="1" :color="`rgba(89, 118, 144, 0.7)`" class="text-white">
+                    <BlurGlassCard class="max-w-xl">
                         <div
                             class="mt-4 text-2xl font-bold underline decoration-wavy decoration-2 decoration-custom-blue underline-offset-8">
                             目標を見つけて、ステップアップ !</div>
                         <div class="mt-4 text-base sm:text-lg font-bold text-center">
                             途中でやりたい事が変わっても<br />積み上げた経験を活かす事で 唯一無二のエンジニアに。
                         </div>
-                        <p class=" max-w-xl text-center text-sm sm:mt-4 sm:text-base font-bold sm:leading-7 leading-7">
+                        <p class="text-center text-sm sm:mt-4 sm:text-base font-bold sm:leading-7 leading-7">
                             人それぞれで目指すものも違えば、向き不向きも有ります。<br
                                 class="hidden md:inline-block" />また、様々な経験の中でやりたい事が変わる事もあるでしょう。<br
                                 class="hidden md:inline-block" />ベイルでは、適宜相談しながら その人に合ったキャリアパスを描いていきたいと考えます。
                         </p>
                     </BlurGlassCard>
                 </div>
-                <div class="mt-4 flex flex-col justify-center items-center">
-                    <NuxtImg class="hidden md:inline-block" width="600" src="/images/recruit/induction/006.png" />
+                <div class="flex flex-col justify-center items-center">
+                    <NuxtImg class="hidden md:inline-block" width="576" src="/images/recruit/induction/006.png" />
                     <NuxtImg class="inline-block md:hidden"  width="343" src="/images/recruit/induction/006.png" />
                 </div>
                 <!-- <div class="mt-6">
@@ -103,7 +103,7 @@
                             <ImageCard src="/images/recruit/induction/008.png"></ImageCard>
                         </div>
                         <div class="w-full sm:w-1/2">
-                            <BlurGlassCard :blur="2" :color="`rgba(0, 0, 0, 0.1)`">
+                            <BlurGlassCard>
                                 <div class="text-2xl font-bold leading-loose">技術資格、業務資格の取得支援</div>
                                 <p class="text-sm sm:mt-4 sm:text-base font-bold sm:leading-7 leading-7">資格取得に向けた支援（受験補助金） 及び、資格認定者に報奨金の支給を行っております。</p>
                                 <ul class="pl-6 list-image-[url(/images/common/check.svg)] text-sm sm:mt-4 sm:text-base font-bold sm:leading-7 leading-7">
@@ -122,7 +122,7 @@
                             <ImageCard src="/images/recruit/induction/009.png"></ImageCard>
                         </div>
                         <div class="w-full sm:w-1/2">
-                            <BlurGlassCard :blur="2" :color="`rgba(0, 0, 0, 0.1)`">
+                            <BlurGlassCard>
                                 <div class="text-2xl font-bold leading-loose">外部研修支援</div>
                                 <p class="text-sm sm:mt-4 sm:text-base font-bold sm:leading-7 leading-7">業務に必要となるスキルを補完する為に、短期的な外部研修を受講して頂く事が有ります。技術的な研修や、コーチング、チームリードなどの技能研修など、身に付けたい（付けて欲しい）スキルに応じて推奨します。
                                 </p>
