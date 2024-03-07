43bd19e3a7f4cfb80b882e09a349970b88fb91eb
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-03-07 16:20:46 +0900

    オフィス紹介ページの作成

--- pages/recruit/office.vue
+++ pages/recruit/office.vue
@@ -26,15 +26,14 @@
                             <p class="text-sm sm:text-base font-bold sm:leading-7 leading-7"><span
                                     class="text-2xl font-bold leading-loose">
                                     山手通りに面した本社オフィス。</span><br />
-                                日比谷線/東急東横線「中目黒駅」から山手通り沿いを5分程歩くと見えてくる、
-                                1階にお寿司屋さん(築地すし好)が入っているビルです。<br />
-                                モダンな外観が目を引きます。
+                                日比谷線/東急東横線「中目黒駅」から山手通り沿いを5分程歩くと見えてくる、1階にお寿司屋さん(築地すし好)が入っているビルです。
                             </p>
                         </BlurGlassCard>
                     </div>
                 </div>
             </div>
         </section>
+
 
         <section class="mt-24">
             <h2 id="recruit-office03"
@@ -48,8 +47,55 @@
                     <div class="w-full">
                         <BlurGlassCard>
                             <p class=" text-sm sm:text-base font-bold sm:leading-7 leading-7">
-                                エレベーターを7階で降りると、すぐ右手に受付があります。<br />
-                                こちらで来訪者の受付を行っています。
+                                7階右手に受付があります。<br />
+                                備え付けの受付電話でご用件をお伝え下さい。
+                            </p>
+                        </BlurGlassCard>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="my-24">
+            <h2 id="recruit-office05"
+                class="pl-16 pr-8 w-fit text-white bg-custom-deepBlue text-4xl font-bold leading-loose">
+                開発ルーム（8階）</h2>
+            <div class="mt-8 px-4 md:mx-auto max-w-xl">
+                <div class="mb-16 flex flex-wrap">
+                    <div class="w-full">
+                        <ImageCard src="/images/recruit/office/005.png" alt="8階 開発ルーム" label="8階 開発ルーム"></ImageCard>
+                    </div>
+                    <div class="w-full">
+                        <BlurGlassCard>
+                            <p class=" text-sm sm:text-base font-bold sm:leading-7 leading-7">
+                                8階は、主に開発フロアとなっており、<br />
+                                受託開発案件などを本社開発メンバーで行っております。
+                            </p>
+                        </BlurGlassCard>
+                    </div>
+                </div>
+                <div class="mb-16 flex flex-wrap">
+                    <div class="w-full">
+                        <ImageCard src="/images/recruit/office/006.png" alt="8階 ミーティングスペース" label="8階 ミーティングスペース"></ImageCard>
+                    </div>
+                    <div class="w-full">
+                        <BlurGlassCard>
+                            <p class=" text-sm sm:text-base font-bold sm:leading-7 leading-7">
+                                8階には開発フロアのほかにミーティングスペースがあり、<br />
+                                プロジェクトの打ち合わせや面談など用途に応じて使用出来るエリアを設けています。
+                            </p>
+                        </BlurGlassCard>
+                    </div>
+                </div>
+                <div class="flex flex-wrap">
+                    <div class="w-full">
+                        <ImageCard src="/images/recruit/office/007.png" alt="8階 グリコボックス" label="8階 グリコボックス"></ImageCard>
+                    </div>
+                    <div class="w-full">
+                        <BlurGlassCard>
+                            <p class=" text-sm sm:text-base font-bold sm:leading-7 leading-7">
+                                8階にはグリコボックスが設置されていて、200円で好きなお菓子が食べれます。<br />
+                                小腹がすいた時や、リフレッシュの際に利用されます。
                             </p>
                         </BlurGlassCard>
                     </div>
@@ -78,52 +124,6 @@
             </div>
         </section>
 
-        <section class="my-24">
-            <h2 id="recruit-office05"
-                class="pl-16 pr-8 w-fit text-white bg-custom-deepBlue text-4xl font-bold leading-loose">
-                開発ルーム（8階）</h2>
-            <div class="mt-8 px-4 md:mx-auto max-w-xl">
-                <div class="mb-16 flex flex-wrap">
-                    <div class="w-full">
-                        <ImageCard src="/images/recruit/office/005.png" alt="8階 開発ルーム" label="8階 開発ルーム"></ImageCard>
-                    </div>
-                    <div class="w-full">
-                        <BlurGlassCard>
-                            <p class=" text-sm sm:text-base font-bold sm:leading-7 leading-7">
-                                8階は、主に開発フロアとなっており、本社在中の社員が仕事をしています。<br />
-                                開発フロアは9階でしたが、2022年にから8階にお引越ししました。
-                            </p>
-                        </BlurGlassCard>
-                    </div>
-                </div>
-                <div class="mb-16 flex flex-wrap">
-                    <div class="w-full">
-                        <ImageCard src="/images/recruit/office/006.png" alt="8階 ミーティングスペース" label="8階 ミーティングスペース"></ImageCard>
-                    </div>
-                    <div class="w-full">
-                        <BlurGlassCard>
-                            <p class=" text-sm sm:text-base font-bold sm:leading-7 leading-7">
-                                8階には開発フロアのほかにミーティングスペースがあり、<br />
-                                プロジェクトの打ち合わせや面談など用途に応じて使用出来るエリアを設けています。
-                            </p>
-                        </BlurGlassCard>
-                    </div>
-                </div>
-                <div class="flex flex-wrap">
-                    <div class="w-full">
-                        <ImageCard src="/images/recruit/office/007.png" alt="8階 グリコボックス" label="8階 グリコボックス"></ImageCard>
-                    </div>
-                    <div class="w-full">
-                        <BlurGlassCard>
-                            <p class=" text-sm sm:text-base font-bold sm:leading-7 leading-7">
-                                8階にはグリコボックスが設置されていて、100円で好きなお菓子が食べれます。<br />
-                                小腹がすいたときやリフレッシュの際に利用されます。
-                            </p>
-                        </BlurGlassCard>
-                    </div>
-                </div>
-            </div>
-        </section>
     </div>
 </template>
 
