821cc167ac7f78d456266ba56762e0853b33cc17
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-03-08 16:10:12 +0900

    コンポーネントのテスト

--- pages/recruit/data.vue
+++ pages/recruit/data.vue
@@ -1,11 +1,19 @@
 <template>
-    <h1>各種データ</h1>
+    <div>
+        <h1>各種データ</h1>
+        <section class="my-24 mx-4">
+            <div class="flex justify-center items-center">
+                <Carousel></Carousel>
+            </div>
+        </section>
+    </div>
 </template>
 
 <script setup lang="ts">
 definePageMeta({
     layout: 'double-column-sidebar'
 })
+import Carousel from '~/components/Carousel.vue'
 </script>
 
 <style scoped></style>
