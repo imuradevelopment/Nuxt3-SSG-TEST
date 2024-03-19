d4b4e662d4825176101760ecfd35febe0e8e1a3c
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-03-19 13:07:35 +0900

    メタデータのテスト

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,7 +25,10 @@ export default defineNuxtConfig({
             charset: "utf-8",
             viewport: "width=device-width, initial-scale=1",
             title: "株式会社ベイル",
-            meta: [{ name: "description", content: "リニューアルサイト" }],
+            meta: [
+                { name: "description", content: "リニューアルサイト" },
+                { name: "keywords", content: "a5b3878fa5c1b85e9db58feb9b6e3449" },
+            ],
         },
     },
     nitro: {
