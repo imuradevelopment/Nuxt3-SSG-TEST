40a26df1a42682864c63433cbf7ab9c215963f9e
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-04-03 11:29:28 +0900

    QAページの実装

--- pages/recruit/qa.vue
+++ pages/recruit/qa.vue
@@ -24,21 +24,44 @@
                     <hr class="mx-auto mb-4 w-12 border-t-4 border-custom-deepBlue">
                 </div>
                 <div>
-                    <ClearGlassMaskCard class="p-4 mb-4" :rounded="false" :opacity="0.1">
-                        <template #content>
-                            <div class="md:mx-4 flex flex-col">
-                                <p
-                                    class="mb-2 pl-4 md:text-3xl lg:text-4xl font-bold text-base sm:leading-loose border-l-4 border-custom-lightBlue">
-                                    <span
-                                        class="text-custom-lightBlue font-bold text-xl md:text-3xl lg:text-4xl sm:leading-loose">Q:</span>
-                                    遠方に住んでいますがリモートでの面接は可能ですか？
-                                </p>
-                                <p class="mb-2 pl-4 sm:text-2xl text-sm sm:leading-loose border-l-4 border-custom-blue">
-                                    最終面接は、来社頂いての面接とさせて頂いていますが、それ以外についてはリモート可能です。
-                                </p>
-                            </div>
-                        </template>
-                    </ClearGlassMaskCard>
+
+                    <div class="mb-4">
+                        <ClearGlassMaskCard class="p-4" :rounded="false" :opacity="0.1">
+                            <template #content>
+                                <div class="md:mx-4 flex flex-col">
+                                    <p
+                                        class="mb-2 pl-4 md:text-3xl lg:text-4xl font-bold text-base sm:leading-loose border-l-4 border-custom-lightBlue">
+                                        <span
+                                            class="text-custom-lightBlue font-bold text-xl md:text-3xl lg:text-4xl sm:leading-loose">Q:</span>
+                                        遠方に住んでいますがリモートでの面接は可能ですか？
+                                    </p>
+                                    <p
+                                        class="mb-2 pl-4 sm:text-2xl text-sm sm:leading-loose border-l-4 border-custom-blue">
+                                        最終面接は、来社頂いての面接とさせて頂いていますが、それ以外についてはリモート可能です。
+                                    </p>
+                                </div>
+                            </template>
+                        </ClearGlassMaskCard>
+                    </div>
+
+                    <div class="mb-4">
+                        <ClearGlassMaskCard class="p-4" :rounded="false" :opacity="0.1">
+                            <template #content>
+                                <div class="md:mx-4 flex flex-col">
+                                    <p
+                                        class="mb-2 pl-4 md:text-3xl lg:text-4xl font-bold text-base sm:leading-loose border-l-4 border-custom-lightBlue">
+                                        <span
+                                            class="text-custom-lightBlue font-bold text-xl md:text-3xl lg:text-4xl sm:leading-loose">Q:</span>
+                                        遠方に住んでいますがリモートでの面接は可能ですか？
+                                    </p>
+                                    <p
+                                        class="mb-2 pl-4 sm:text-2xl text-sm sm:leading-loose border-l-4 border-custom-blue">
+                                        最終面接は、来社頂いての面接とさせて頂いていますが、それ以外についてはリモート可能です。
+                                    </p>
+                                </div>
+                            </template>
+                        </ClearGlassMaskCard>
+                    </div>
                 </div>
             </BlurGlassCard>
         </section>
