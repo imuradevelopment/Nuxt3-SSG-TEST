e681c80084e34c11875c61110af9944cfc006552
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-04-03 11:16:29 +0900

    QAページの実装

--- pages/recruit/qa.vue
+++ pages/recruit/qa.vue
@@ -30,7 +30,7 @@
                                 <p
                                     class="mb-2 pl-4 sm:text-3xl font-bold text-base sm:leading-loose border-l-4 border-custom-lightBlue">
                                     <span
-                                        class="text-custom-lightBlue font-bold text-xl sm:text-4xl sm:leading-loose">Q：</span>
+                                        class="text-custom-lightBlue font-bold text-xl sm:text-4xl sm:leading-loose">Q:</span>
                                     遠方に住んでいますがリモートでの面接は可能ですか？
                                 </p>
                                 <p class="mb-2 pl-4 sm:text-2xl text-sm sm:leading-loose border-l-4 border-custom-blue">
