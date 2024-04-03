5f2b8940bbcbdadf1771fec13da585dc82fee48e
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-04-03 10:39:34 +0900

    QAページの実装

--- pages/recruit/qa.vue
+++ pages/recruit/qa.vue
@@ -14,13 +14,42 @@
                     皆様の良く有る質問を纏めました。</p>
             </div>
         </section>
+
+        <section class="mb-12 md:mb-24">
+            <BlurGlassCard>
+                <div>
+                    <h2 id="recruit-qa02" class="text-center text-custom-deepBlue text-4xl font-bold leading-loose">
+                        採用について
+                    </h2>
+                    <hr class="mx-auto mb-4 w-12 border-t-4 border-custom-deepBlue">
+                </div>
+                <div>
+                    <ClearGlassMaskCard class="p-4" :rounded="false" :opacity="0.1">
+                        <template #content>
+                            <div class="flex flex-col">
+                                <p class="mb-2 pl-4 text-3xl leading-loose border-l-4 border-custom-lightBlue">
+                                    <span class="text-custom-lightBlue text-4xl leading-loose">Q：</span>
+                                    遠方に住んでいますがリモートでの面接は可能ですか？
+                                </p>
+                                <p class="mb-2 pl-4 text-2xl leading-loose border-l-4 border-custom-blue">
+                                    最終面接は、来社頂いての面接とさせて頂いていますが、それ以外についてはリモート可能です。
+                                </p>
+                            </div>
+                        </template>
+                    </ClearGlassMaskCard>
+                </div>
+            </BlurGlassCard>
+        </section>
+
     </div>
 </template>
 
 <script setup lang="ts">
-definePageMeta({
-    layout: 'double-column-sidebar'
-})
-</script>
+            definePageMeta({
+            layout: 'double-column-sidebar'
+            })
+import BlurGlassCard from '~/components/BlurGlassCard.vue'
+import ClearGlassMaskCard from '~/components/ClearGlassMaskCard.vue'
+            </script>
 
-<style scoped></style>
+            <style scoped></style>
