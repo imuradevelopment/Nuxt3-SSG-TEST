5d240018eff19e8d16d50b6e362b9eb4ced9a2f2
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-04-09 17:59:28 +0900

    社員紹介ページの作成

---
+++ components/CarouselEmployee.vue
@@ -0,0 +1,322 @@
+<template>
+    <div class="grid-carousel">
+
+        <div class="scene">
+            <div class="carousel">
+                <!-- サムネイル画像をv-forで繰り返し表示 -->
+                <div class="face" v-for="(pic, index) in pics" :key="index" :id="index.toString()"
+                    @click="selectItem(index)" :style="{ backgroundImage: 'url(' + pic.src + ')' }"></div>
+            </div>
+        </div>
+
+        <div class="hero-wrapper">
+            <!-- ヒーローイメージとして選択された画像を表示 -->
+            <div class="hero" :style="heroStyle">
+                <div class="introduction h-full" v-html="pics[currentHeroNumber].introduction"></div>
+            </div>
+        </div>
+
+        <div class="message">
+            <div class="my-6">
+                <ClearGlassMaskCard class="p-4" :rounded="false" :opacity="0.1">
+                    <template #content>
+                        <div class="md:mx-4 flex flex-col">
+                            <p
+                                class="mb-2 pl-4 md:text-3xl font-bold text-base md:leading-loose border-l-4 border-custom-lightBlue">
+                                <span
+                                    class="text-custom-lightBlue font-bold text-xl md:text-3xl md:leading-loose">Q:</span>
+                                現在の仕事について教えて下さい。
+                            </p>
+                            <p class="mb-2 pl-4 text-sm md:text-xl md:leading-loose border-l-4 border-custom-blue">
+                            <div v-html="pics[currentHeroNumber].message1"></div>
+                            </p>
+                        </div>
+                    </template>
+                </ClearGlassMaskCard>
+            </div>
+            <div class="mb-6">
+                <ClearGlassMaskCard class="p-4" :rounded="false" :opacity="0.1">
+                    <template #content>
+                        <div class="md:mx-4 flex flex-col">
+                            <p
+                                class="mb-2 pl-4 md:text-3xl font-bold text-base md:leading-loose border-l-4 border-custom-lightBlue">
+                                <span
+                                    class="text-custom-lightBlue font-bold text-xl md:text-3xl md:leading-loose">Q:</span>
+                                この仕事の好きなところはどこですか？
+                            </p>
+                            <p class="mb-2 pl-4 text-sm md:text-xl md:leading-loose border-l-4 border-custom-blue">
+                            <div v-html="pics[currentHeroNumber].message2"></div>
+                            </p>
+                        </div>
+                    </template>
+                </ClearGlassMaskCard>
+            </div>
+            <div class="mb-6">
+                <ClearGlassMaskCard class="p-4" :rounded="false" :opacity="0.1">
+                    <template #content>
+                        <div class="md:mx-4 flex flex-col">
+                            <p
+                                class="mb-2 pl-4 md:text-3xl font-bold text-base md:leading-loose border-l-4 border-custom-lightBlue">
+                                <span
+                                    class="text-custom-lightBlue font-bold text-xl md:text-3xl md:leading-loose">Q:</span>
+                                就職活動へのアドバイスをお願いします。
+                            </p>
+                            <p class="mb-2 pl-4 text-sm md:text-xl md:leading-loose border-l-4 border-custom-blue">
+                            <div v-html="pics[currentHeroNumber].message3"></div>
+                            </p>
+                        </div>
+                    </template>
+                </ClearGlassMaskCard>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import BlurGlassCard from './BlurGlassCard.vue';
+import ClearGlassMaskCard from '~/components/ClearGlassMaskCard.vue'
+
+// propsでpicsをオブジェクトの配列として定義
+const props = defineProps({
+    pics: {
+        type: Array as () => {
+            src: string;
+            alt?: string;
+            introduction: string;
+            message1: string;
+            message2: string;
+            message3: string;
+            tileColor?: string;
+            shadowColor?: string;
+            bgPosition?: string;
+        }[],
+        required: true
+    }
+});
+
+const currentPhotoNumber = ref<number>(0);
+const currentHeroNumber = ref<number>(0);
+const heroStyle = computed(() => ({
+    backgroundImage: `url(${props.pics[currentHeroNumber.value].src})`,
+    backgroundSize: 'cover',
+    backgroundPosition: `${props.pics[currentHeroNumber.value].bgPosition}`
+}));
+
+let carousel: HTMLElement;
+let hero: HTMLElement;
+let message: HTMLElement;
+
+onMounted(() => {
+    carousel = document.querySelector('.carousel') as HTMLElement;
+    hero = document.querySelector('.hero') as HTMLElement;
+    message = document.querySelector('.message') as HTMLElement;
+
+    const scrollableElement = document.querySelector('.carousel') as HTMLElement;
+
+    scrollableElement.addEventListener('wheel', (e) => {
+        e.preventDefault();
+        scrollableElement.scrollLeft += e.deltaY;
+    });
+
+    itemFocus(currentPhotoNumber.value);
+});
+
+const selectItem = (index: number) => {
+
+    itemFocus(index);
+
+    setTimeout(() => {
+        currentHeroNumber.value = currentPhotoNumber.value;
+    }, 500);
+
+    hero.classList.add('switching');
+    hero.addEventListener('animationend', () => {
+        hero.classList.remove('switching');
+    });
+
+    message.classList.add('switching');
+    message.addEventListener('animationend', () => {
+        message.classList.remove('switching');
+    });
+}
+
+const itemFocus = (index: number) => {
+    let faceBefore = carousel.children[currentPhotoNumber.value] as HTMLElement;
+    faceBefore.style.boxShadow = `none`;
+    currentPhotoNumber.value = index;
+    let face = carousel.children[currentPhotoNumber.value] as HTMLElement;
+    face.style.boxShadow = `0 0 5px var(--custom-color-blue),2px 2px 3px var(--custom-color-blue),-2px -2px 3px var(--custom-color-blue)`;
+}
+</script>
+
+<style scoped>
+.grid-carousel {
+    display: grid;
+    grid-template-areas:
+        "images"
+        "hero"
+        "message";
+    grid-template-columns: minmax(300px, calc(1280px - 160px));
+    grid-template-rows: calc(7rem + 8px + 12px) 600px auto;
+    gap: 0.5rem;
+}
+@media screen and (max-width: 768px) {
+    .grid-carousel {
+        display: grid;
+        grid-template-areas:
+            "images"
+            "hero"
+            "message";
+        grid-template-columns: minmax(300px, calc(1280px - 160px));
+        grid-template-rows: calc(7rem + 8px + 12px) 450px auto;
+        gap: 0.5rem;
+    }
+}
+
+.hero-wrapper {
+    grid-area: hero;
+    display: flex;
+    filter: drop-shadow(0px 0px 2px var(--custom-color-deepGray)) drop-shadow(2px 2px 1px var(--custom-color-deepGray)) drop-shadow(-2px -2px 1px var(--custom-color-deepGray));
+}
+
+.hero {
+    margin: auto;
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    position: relative;
+    border-radius: 5px;
+}
+
+.hero::after {
+    background: transparent;
+    content: "";
+    position: absolute;
+    transition: all 1s linear;
+}
+
+.switching {
+    animation: leaving 1s ease-in-out forwards;
+}
+
+@keyframes leaving {
+    0% {
+        clip-path: polygon(0 100%, 0 0, 14.29% 0, 14.29% 0, 28.58% 0, 28.58% 0, 42.87% 0, 42.87% 0, 57.16% 0, 57.16% 0, 71.45% 0, 71.45% 0, 85.74% 0, 85.74% 0, 100% 0, 100% 100%, 0 100%);
+    }
+
+    25% {
+        clip-path: polygon(0 100%, 0 75%, 14.29% 75%, 14.29% 55%, 28.58% 55%, 28.58% 25%, 42.87% 25%, 42.87% 0, 57.16% 0, 57.16% 25%, 71.45% 25%, 71.45% 55%, 85.74% 55%, 85.74% 75%, 100% 75%, 100% 100%, 0 100%);
+    }
+
+    50% {
+        clip-path: polygon(0 100%, 0 100%, 14.29% 100%, 14.29% 100%, 28.58% 100%, 28.58% 100%, 42.87% 100%, 42.87% 100%, 57.16% 100%, 57.16% 100%, 71.45% 100%, 71.45% 100%, 85.74% 100%, 85.74% 100%, 100% 100%, 100% 100%, 0 100%);
+    }
+
+    51% {
+        clip-path: polygon(0 0, 0 0, 100% 0, 100% 0, 85.74% 0, 85.74% 0, 71.45% 0, 71.45% 0, 57.16% 0, 57.16% 0, 42.87% 0, 42.87% 0, 28.58% 0, 28.58% 0, 14.29% 0, 14.29% 0, 0 0);
+    }
+
+    76% {
+        clip-path: polygon(0 25%, 0 0, 100% 0, 100% 25%, 85.74% 25%, 85.74% 45%, 71.45% 45%, 71.45% 75%, 57.16% 75%, 57.16% 100%, 42.87% 100%, 42.87% 75%, 28.58% 75%, 28.58% 45%, 14.29% 45%, 14.29% 25%, 0 25%);
+    }
+
+    100% {
+        clip-path: polygon(0 100%, 0 0, 100% 0, 100% 100%, 85.74% 100%, 85.74% 100%, 71.45% 100%, 71.45% 100%, 57.16% 100%, 57.16% 100%, 42.87% 100%, 42.87% 100%, 28.58% 100%, 28.58% 100%, 14.29% 100%, 14.29% 100%, 0 100%);
+    }
+}
+
+.scene {
+    grid-area: images;
+}
+
+.carousel {
+    display: flex;
+    height: 100%;
+    justify-content: space-between;
+    align-items: center;
+    gap: 1rem;
+    overflow-x: auto;
+    flex-shrink: 0;
+    padding-left: 6px;
+    padding-right: 6px;
+}
+
+.face {
+    flex-shrink: 0;
+    width: 7rem;
+    height: 7rem;
+    top: 0;
+    left: 5px;
+    background-size: cover;
+    background-position: center;
+    opacity: 1;
+    border: 2px solid var(--custom-color-blue);
+    border-radius: 5px;
+}
+
+.main-text-card {
+    position: absolute;
+    top: 0;
+    left: 0;
+    /* max-width: 300px; 適宜調整 */
+}
+
+.card {
+    box-shadow: none !important;
+}
+
+.sub-text {
+    position: absolute;
+    bottom: 1rem;
+    left: 50%;
+    transform: translateX(-50%);
+    color: white;
+    font-size: 1em;
+    margin-bottom: 1rem;
+    background: rgba(0, 0, 0, 0.5);
+    padding: 0.5rem 1rem;
+    border-radius: 10px;
+}
+
+.sub-text-center-bottom {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 1rem;
+    top: auto;
+}
+
+/* スクロールバー全体のスタイル */
+.carousel::-webkit-scrollbar {
+    width: 8px;
+    /* スクロールバーの幅を細く */
+    height: 8px;
+}
+
+/* スクロールバーのトラック（背景部分）のスタイル */
+.carousel::-webkit-scrollbar-track {
+    background: rgba(0, 0, 0, 0.1);
+    /* 背景色を透明度の低い黒に */
+}
+
+/* スクロールバーのサム（動かす部分）のスタイル */
+.carousel::-webkit-scrollbar-thumb {
+    background-color: rgba(0, 0, 0, 0.2);
+    /* サムの色を透明度のやや高い黒に */
+    border-radius: 4px;
+    /* 角を丸く */
+}
+
+/* スクロールバーのサムにホバーしたときのスタイル */
+.carousel::-webkit-scrollbar-thumb:hover {
+    background-color: rgba(0, 0, 0, 0.3);
+    /* ホバー時の色を少し濃く */
+}
+
+.message {
+    grid-area: message;
+}
+</style>
--- pages/recruit/employee.vue
+++ pages/recruit/employee.vue
@@ -14,6 +14,28 @@
                     ベイルの先輩社員から貰った<br class="inline-block md:hidden" />コメントを紹介します。</p>
             </div>
         </section>
+
+        <section class="mb-12 md:mb-24">
+            <h2 id="recruit-employee02 mb-4"
+                class="pl-16 pr-8 w-fit text-white bg-custom-deepBlue text-4xl font-bold leading-loose">
+                インタビュー</h2>
+            <div class=" mx-4">
+                <CarouselEmployee :pics="[
+                    {
+                        src: '/images/recruit/employee/003.jpeg',
+                        alt: 'オフィス外観',
+                        introduction: `<div class='flex h-full items-center justify-end'><div class='h-full bg-custom-deepBlue/70 text-white flex justify-center items-center'><div class='p-4 text-2xl font-bold leading-loose text-center'>システムエンジニア<br />Y.K<br />システム部<br />東京理科大学<br />理工学部情報学科出身</div></div></div>`,
+                        message1: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>山手通りに面した本社オフィス。<br />日比谷線/東急東横線「中目黒駅」から山手通り沿いを5分程歩くと見えてくる、<br class='hidden md:inline-block' />1階にお寿司屋さん(築地すし好)が入っているビルです。</p>`,
+                        message2: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>山手通りに面した本社オフィス。<br />日比谷線/東急東横線「中目黒駅」から山手通り沿いを5分程歩くと見えてくる、<br class='hidden md:inline-block' />1階にお寿司屋さん(築地すし好)が入っているビルです。</p>`,
+                        message3: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>山手通りに面した本社オフィス。<br />日比谷線/東急東横線「中目黒駅」から山手通り沿いを5分程歩くと見えてくる、<br class='hidden md:inline-block' />1階にお寿司屋さん(築地すし好)が入っているビルです。</p>`,
+                        tileColor: 'rgba(0, 37, 92, 0.1)',
+                        shadowColor: 'rgb(0 0 0 / 0.5)',
+                        bgPosition: 'center'
+                    },
+                    // 他の画像オブジェクト...
+                ]"></CarouselEmployee>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -21,6 +43,7 @@
 definePageMeta({
     layout: 'double-column-sidebar'
 })
+import CarouselEmployee from '~/components/CarouselEmployee.vue'
 </script>
 
 <style scoped></style>
