02007e5e5f69c37b3aec2cc58cd377fe1b2c4e7f
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-04-10 10:48:51 +0900

    社員紹介ページの作成

--- pages/recruit/employee.vue
+++ pages/recruit/employee.vue
@@ -22,18 +22,7 @@
             <div class=" mx-4">
                 <CarouselEmployee :pics="[
                     {
-                        src: '/images/recruit/employee/003.jpeg',
-                        alt: '人物写真Y.K',
-                        introduction: `<div class='flex h-full items-end justify-center md:items-center md:justify-end'><div class='h-auto w-full md:h-full md:w-auto bg-custom-deepBlue/70 text-white flex justify-center items-center'><div class='p-1 md:p-4 text-xs md:text-2xl font-bold leading-5 md:leading-loose text-center'>システムエンジニア<br />Y.K<br />システム部<br />東京理科大学<br />理工学部情報学科出身</div></div></div>`,
-                        message1: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>リリース済システムの保守開発の検証、及びシステムの提案書作成やデータ活用の一環として、企業内に存在するデータ品質の確保・向上に携わっています。</p>`,
-                        message2: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>システム開発は、ものづくりです。さまざまな工程がありますが、どの工程もとても大事な作業です。例えば、テスト工程で想定と違う結果となってしまったとき、その問題解析や修正などを素早く対応しなければなりません。1人でわからなければチーム全体で協力して取り組み、修正後に再度テストを実施して正しい結果が出力されることを確認します。このような一つひとつの小さな作業の積み重ねによりシステムが完成し、お客様に納品することができたその時の達成感は、とても印象的です。</p>`,
-                        message3: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>就職活動の際には自分が持っている軸を大事にしてください。入社してから思っていたのとは違うなというのはとてももったいないです。私の場合は同じ企業でも面接毎に自分のやりたいことが本当にできるのか面接担当者に聞きました。選んだ会社が本当に自分とあっているのか、これでもかというくらい調べて納得のいく結果を得られるよう頑張ってください。</p>`,
-                        tileColor: 'rgba(0, 37, 92, 0.1)',
-                        shadowColor: 'rgb(0 0 0 / 0.5)',
-                        bgPosition: 'center'
-                    },
-                    {
-                        src: '/images/recruit/employee/003.jpeg',
+                        src: '/images/recruit/employee/003.png',
                         alt: '人物写真Y.K',
                         introduction: `<div class='flex h-full items-center justify-end'><div class='h-full w-auto bg-custom-deepBlue/70 text-white flex justify-center items-center'><div class='p-1 md:p-4 text-xs md:text-2xl font-bold leading-5 md:leading-loose text-center'>システムエンジニア<br />Y.K<br />システム部<br />東京理科大学<br />理工学部情報学科出身</div></div></div>`,
                         message1: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>リリース済システムの保守開発の検証、及びシステムの提案書作成やデータ活用の一環として、企業内に存在するデータ品質の確保・向上に携わっています。</p>`,
@@ -63,7 +52,7 @@
                         message3: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>就職活動の際には自分が持っている軸を大事にしてください。入社してから思っていたのとは違うなというのはとてももったいないです。私の場合は同じ企業でも面接毎に自分のやりたいことが本当にできるのか面接担当者に聞きました。選んだ会社が本当に自分とあっているのか、これでもかというくらい調べて納得のいく結果を得られるよう頑張ってください。</p>`,
                         tileColor: 'rgba(0, 37, 92, 0.1)',
                         shadowColor: 'rgb(0 0 0 / 0.5)',
-                        bgPosition: 'right'
+                        bgPosition: 'right top'
                     },
                     {
                         src: '/images/recruit/employee/006.jpg',
@@ -74,7 +63,7 @@
                         message3: `<p class='message text-sm sm:text-base font-bold sm:leading-7 leading-7'>就職活動の際には自分が持っている軸を大事にしてください。入社してから思っていたのとは違うなというのはとてももったいないです。私の場合は同じ企業でも面接毎に自分のやりたいことが本当にできるのか面接担当者に聞きました。選んだ会社が本当に自分とあっているのか、これでもかというくらい調べて納得のいく結果を得られるよう頑張ってください。</p>`,
                         tileColor: 'rgba(0, 37, 92, 0.1)',
                         shadowColor: 'rgb(0 0 0 / 0.5)',
-                        bgPosition: 'right'
+                        bgPosition: 'right top'
                     },
                     {
                         src: '/images/recruit/employee/007.jpg',
