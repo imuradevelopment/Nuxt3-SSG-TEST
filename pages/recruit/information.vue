a4dfea77dd66158972fd4bfcdbe28f13af4aa44b
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-05-15 19:36:17 +0900

    募集情報ページの作成

--- pages/recruit/information.vue
+++ pages/recruit/information.vue
@@ -15,6 +15,14 @@
                     新卒採用の募集内容をお伝えします。</p>
             </div>
         </section>
+
+        <section>
+            <el-tabs v-model="activeName" type="card" class="tabs" @tab-click="handleClick">
+                <el-tab-pane label="新卒採用" name="first">User</el-tab-pane>
+                <el-tab-pane label="キャリア採用" name="second">Config</el-tab-pane>
+                <el-tab-pane label="契約社員採用" name="third">Role</el-tab-pane>
+            </el-tabs>
+        </section>
     </div>
 </template>
 
@@ -22,6 +30,50 @@
 definePageMeta({
     layout: 'double-column-sidebar'
 })
+import { ref } from 'vue'
+import type { TabsPaneContext } from 'element-plus'
+
+const activeName = ref('first')
+
+const handleClick = (tab: TabsPaneContext, event: Event) => {
+    console.log(tab, event)
+}
 </script>
 
-<style scoped></style>
+<style>
+.tabs>.el-tabs__content {
+    padding: 32px;
+    color: #6b778c;
+    font-size: 32px;
+    font-weight: 600;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__nav {
+    border: none!important;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__item:first-child {
+    border-left: 1px solid var(--el-border-color-light) !important;
+    margin-right: 1rem !important;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__item:last-child {
+    margin-left:1rem!important;
+}
+
+.el-tabs--card>.el-tabs__header .el-tabs__item {
+    border-bottom: 1px solid #0000 !important;
+    border-left: 1px solid var(--el-border-color-light)!important;
+    border-right: 1px solid var(--el-border-color-light)!important;
+    border-top: 1px solid var(--el-border-color-light)!important;
+    border-top-left-radius: 0.375rem !important;
+    border-top-right-radius: 0.375rem !important;
+    transition: color var(--el-transition-duration) var(--el-transition-function-ease-in-out-bezier), padding var(--el-transition-duration) var(--el-transition-function-ease-in-out-bezier)!important;
+}
+
+.el-tabs__item.is-active,
+.el-tabs__item:hover {
+    color: white !important;
+    background-color: var(--custom-color-deepBlue)!important;
+}
+</style>
