74ab34f76b0b20bea2454d97f099e52740394563
イムラ <67383715+imuradevelopment@users.noreply.github.com>
2024-06-25 08:46:51 +0000

    お問い合わせページの作成

---
+++ components/CustomArrowButton_bug.vue
@@ -0,0 +1,404 @@
+<!-- 
+■■■■■■ 使い方 ■■■■■
+    背景色が明るい場合は「colorType」プロパティの指定無し
+        <div class="p-4 bg-gray-200">
+            <CustomArrowButton arrowType="outer" to="https://www.google.co.jp/"><template #buttonText>別タブリンクに使用</template></CustomArrowButton>
+            <CustomArrowButton arrowType="transition" to="/company"><template #buttonText>ページ遷移に使用</template></CustomArrowButton>
+            <CustomArrowButton arrowType="inner" to="/company#company01"><template #buttonText>ターゲット要素への遷移、ページ内遷移に使用</template></CustomArrowButton>
+        </div>
+
+    背景色が暗い場合は「colorType」プロパティに「yellow」を指定する
+        <div class="p-4 bg-custom-deepBlue">
+            <CustomArrowButton arrowType="outer" colorType="yellow" to="https://www.google.co.jp/"><template #buttonText>別タブリンクに使用</template></CustomArrowButton>
+            <CustomArrowButton arrowType="transition" colorType="yellow" to="/company"><template #buttonText>ページ遷移に使用</template></CustomArrowButton>
+            <CustomArrowButton arrowType="inner" colorType="yellow" to="/company#company01"><template #buttonText>ターゲット要素への遷移、ページ内遷移に使用</template></CustomArrowButton>
+        </div>
+
+■■■■■■ インポート等 ■■■■■
+<script setup lang="ts">
+//ここではコンポーネント内の「<script setup lang="ts">」でインポートし、タグ名を「CustomArrowButton」としている。
+import CustomArrowButton from '~/components/CustomArrowButton.vue'
+// 使いたい人は下記のようにiconをインポートしてボタンテキストに使用できます。
+// 参考：https://element-plus.org/en-US/component/icon.html#icon-collection
+import {
+    EditPen,
+} from '@element-plus/icons-vue'
+</script>
+
+■■■■■■ アイコンを使用する場合の例 ■■■■■
+<CustomArrowButton arrowType="transition" to="/company"><template #buttonText><EditPen class="h-6 w-6 inline" />ベイルの特徴</template></CustomArrowButton>
+ -->
+
+<template>
+    <!--
+        コンポーネントの使い方：
+        背景色が明るい場合は「colorType」プロパティの指定無し
+        背景色が暗い場合は「colorType」プロパティに「yellow」を指定する
+    -->
+    <NuxtLink v-if="isOuter" target="_blank" rel="noopener noreferrer" :href="props.to" class="
+            w-fit 
+            relative 
+            inline-flex 
+            items-center 
+            justify-center 
+            px-6 
+            py-2 
+            text-lg 
+            cursor-pointer 
+            transition-all 
+            duration-200 
+            ease-in-out 
+            before:absolute 
+            before:inset-0 
+            before:transform 
+            before:-scale-x-0 
+            before:origin-left 
+            before:transition-transform 
+            before:duration-200 
+            before:ease-in-out 
+            before:rounded-full 
+            ml-2  
+            after:transform 
+            after:translate-x-0 
+            after:transition-transform 
+            after:duration-200 
+            after:ease-in-out 
+            hover:border-transparent 
+            hover:before:scale-x-100 
+            origin-left 
+            hover:after:translate-x-4 
+            rounded-l-full 
+            rounded-r-full 
+            hover:rounded-r-full 
+            overflow-hidden
+            shadow-md
+        " :style="`--after-content: '${afterContent}'`">
+        <span class="relative z-10">
+            <slot name="buttonText">MORE</slot>
+        </span>
+    </NuxtLink>
+    <a @click="handleClick" v-if="isInner" :href="props.to" class="
+            w-fit 
+            relative 
+            inline-flex 
+            items-center 
+            justify-center 
+            px-6 
+            py-2 
+            text-lg 
+            cursor-pointer 
+            transition-all 
+            duration-200
+            ease-in-out 
+            before:absolute 
+            before:inset-0 
+            before:transform 
+            before:-scale-x-0 
+            before:origin-left 
+            before:transition-transform 
+            before:duration-200 
+            before:ease-in-out 
+            before:rounded-full 
+            ml-2  
+            after:transform 
+            after:translate-x-0 
+            after:transition-transform 
+            after:duration-200 
+            after:ease-in-out 
+            hover:border-transparent 
+            hover:before:scale-x-100 
+            origin-left 
+            hover:after:translate-x-4 
+            rounded-l-full 
+            rounded-r-full 
+            hover:rounded-r-full 
+            overflow-hidden
+            shadow-md
+        " :style="`--after-content: '${afterContent}'`">
+        <span class="relative z-10">
+            <slot name="buttonText">MORE</slot>
+        </span>
+    </a>
+    <a @click="handleClick" v-if="!isOuter && !isInner" :to="props.to" class="
+            w-fit 
+            relative 
+            inline-flex 
+            items-center 
+            justify-center 
+            px-6 
+            py-2 
+            text-lg 
+            cursor-pointer 
+            transition-all 
+            duration-200 
+            ease-in-out 
+            before:absolute 
+            before:inset-0 
+            before:transform 
+            before:-scale-x-0 
+            before:origin-left 
+            before:transition-transform 
+            before:duration-200 
+            before:ease-in-out 
+            before:rounded-full 
+            ml-2  
+            after:transform 
+            after:translate-x-0 
+            after:transition-transform 
+            after:duration-200 
+            after:ease-in-out 
+            hover:border-transparent 
+            hover:before:scale-x-100 
+            origin-left 
+            hover:after:translate-x-4 
+            rounded-l-full 
+            rounded-r-full 
+            hover:rounded-r-full 
+            overflow-hidden
+            shadow-md
+        " :style="`--after-content: '${afterContent}'`">
+        <span class="relative z-10">
+            <slot name="buttonText">MORE</slot>
+        </span>
+    </a>
+</template>
+
+
+<script setup lang="ts">
+import { useScrollToTarget } from '~/composables/useScrollToTarget'
+
+// コンポーネントに渡されるプロパティ
+const props = defineProps({
+    to: {
+        type: String,
+        required: true,
+    },
+    height: {
+        type: String,
+        required: false,
+        validator: (value: string) => {
+            return ['auto'].includes(value)
+        }
+    },
+    arrowType: {
+        type: String,
+        required: true,
+        validator: (value: string) => {
+            return ['outer', 'transition', 'inner'].includes(value)
+        }
+    },
+    colorType: {
+        type: String,
+        required: false,
+        validator: (value: string) => {
+            return ['blue-bg-white', 'yellow', 'white'].includes(value)
+        }
+    }
+})
+
+// ボタンの高さを計算する
+const height = computed(() => {
+    switch (props.height) {
+        case 'auto':
+            return 'auto'
+        default:
+            return 'auto'
+    }
+})
+
+
+
+// ボタンの矢印部分の表示内容を計算する
+const afterContent = computed(() => {
+    switch (props.arrowType) {
+        case 'outer':
+            return '↗'
+        case 'transition':
+            return '→'
+        case 'inner':
+            return '↓'
+        default:
+            return ''
+    }
+})
+
+// 背景の色を計算する
+const backgroundcolor = computed(() => {
+    switch (props.colorType) {
+        case 'blue-bg-white':
+            return 'white'
+        case 'yellow-bg-white':
+            return 'white'
+        case 'yellow':
+            return 'transparent'
+        case 'white':
+            return 'transparent'
+        default:
+            return 'transparent'
+    }
+})
+
+// 背景の色を計算する（前景の色の補完）
+const beforebackgroundcolor = computed(() => {
+    switch (props.colorType) {
+        case 'yellow':
+            return 'rgba(231 198 148)'
+        case 'white':
+            return 'white'
+        default:
+            return 'rgb(0 10 135)'
+    }
+})
+
+// テキストの色を計算する
+const color = computed(() => {
+    switch (props.colorType) {
+        case 'yellow':
+            return 'rgba(231 198 148)'
+        case 'white':
+            return 'white'
+        default:
+            return 'rgb(0 10 135)'
+    }
+})
+
+// ホバー時のテキスト色を計算する
+const hovercolor = computed(() => {
+    switch (props.colorType) {
+        case 'yellow':
+            return 'rgb(0 10 135)'
+        case 'white':
+            return 'rgb(0 10 135)'
+        default:
+            return 'rgb(231 198 148)'
+    }
+})
+
+// ボーダーの幅を計算する
+const borderwidth = computed(() => {
+    switch (props.colorType) {
+        case 'yellow':
+            return '2px'
+        case 'white':
+            return '2px'
+        default:
+            return '2px'
+    }
+})
+
+// ボーダーの色を計算する
+const bordercolor = computed(() => {
+    switch (props.colorType) {
+        case 'yellow':
+            return 'rgba(231 198 148)'
+        case 'white':
+            return 'white'
+        default:
+            return 'rgb(0 10 135)'
+    }
+})
+
+// アウトラインのスタイルを計算する
+const outline = computed(() => {
+    switch (props.colorType) {
+        case 'yellow':
+            return '2px solid transparent'
+        case 'white':
+            return '2px solid transparent'
+        default:
+            return '2px solid transparent'
+    }
+})
+
+// ホバー時のアウトラインのスタイルを計算する
+const hoveroutline = computed(() => {
+    switch (props.colorType) {
+        case 'yellow':
+            return '2px solid transparent'
+        case 'white':
+            return '2px solid transparent'
+        default:
+            return '2px solid transparent'
+    }
+})
+
+// アウトラインのオフセットを計算する
+const outlineoffset = computed(() => {
+    switch (props.colorType) {
+        case 'yellow':
+            return '2px'
+        case 'white':
+            return '2px'
+        default:
+            return '2px'
+    }
+})
+
+// ボタンのタイプが「outer」かどうかを判定
+const isOuter = computed(() => {
+    switch (props.arrowType) {
+        case 'outer':
+            return true;
+        default:
+            return false;
+    }
+})
+
+
+// ボタンのタイプが「inner」かどうかを判定
+const isInner = computed(() => {
+    switch (props.arrowType) {
+        case 'inner':
+            return true;
+        default:
+            return false;
+    }
+})
+
+const router = useRouter();
+// クリックイベントを処理する関数
+const handleClick = (event: Event) => {
+    // デフォルトの遷移動作を防ぐ
+    event.preventDefault()
+    // 遷移先のURLを取得
+    const targetPage = props.to
+    // リンク要素を取得
+    const link = event.currentTarget as HTMLElement
+    // transitionendイベントのリスナーを追加
+    link.addEventListener("transitionend", () => {
+        // アニメーションが終わったら遷移する
+        router.push(targetPage)
+    })
+    useScrollToTarget()
+}
+</script>
+
+<style scoped>
+a:before {
+    background-color: v-bind(beforebackgroundcolor);
+}
+
+a {
+    border-color: v-bind(bordercolor);
+    color: v-bind(color);
+    border-width: v-bind(borderwidth);
+    outline: v-bind(outline);
+    outline-offset: v-bind(outlineoffset);
+    background-color: v-bind(backgroundcolor);
+    height: v-bind(height);
+}
+
+a:after {
+    content: var(--after-content);
+    margin-left: 0.25rem;
+}
+
+a:hover {
+    color: v-bind(hovercolor);
+    border: v-bind(hoveroutline);
+}
+
+a:hover:after {
+    content: var(--after-content);
+}
+</style>
--- public/common/send-email.php
+++ public/common/send-email.php
@@ -6,16 +6,10 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 // バリデーション
 $errors = [];
-if (empty($data['lastName'])) {
-    $errors[] = '氏名を入力してください';
-} elseif (strlen($data['lastName']) > 60) {
-    $errors[] = '氏名は60文字以内で入力してください';
-}
-
-if (strlen($data['kanaName']) > 180) {
-    $errors[] = '氏名（フリガナ）は180文字以内で入力してください';
-} elseif (!empty($data['kanaName']) && !preg_match('/^[ァ-ヶー]+$/u', $data['kanaName'])) {
-    $errors[] = '氏名（フリガナ）はカタカナのみで入力してください';
+if (empty($data['name'])) {
+    $errors[] = '名前を入力してください';
+} elseif (strlen($data['name']) > 60) {
+    $errors[] = '名前は60文字以内で入力してください';
 }
 
 if (empty($data['email'])) {
@@ -24,14 +18,6 @@ if (empty($data['email'])) {
     $errors[] = 'メールアドレスは254文字以内で入力してください';
 } elseif (!filter_var($data['email'], FILTER_VALIDATE_EMAIL)) {
     $errors[] = 'メールアドレスは正しい形式で入力してください';
-}
-
-if (empty($data['phone'])) {
-    $errors[] = '電話番号を入力してください';
-} elseif (strlen($data['phone']) > 13) {
-    $errors[] = '電話番号は13文字以内で入力してください';
-} elseif (!preg_match('/^\d{2,4}-\d{2,4}-\d{4}$/', $data['phone'])) {
-    $errors[] = '電話番号は正しい形式で入力してください';
 }
 
 if (empty($data['inquiryContent'])) {
@@ -46,10 +32,8 @@ if (!empty($errors)) {
 // メール送信
 $to = 'yutan030@gmail.com'; // 送信先のメールアドレス
 $subject = 'お問い合わせフォームからのメッセージ';
-$message = "氏名: {$data['lastName']}\n";
-$message .= "氏名（フリガナ）: {$data['kanaName']}\n";
+$message = "名前: {$data['name']}\n";
 $message .= "メールアドレス: {$data['email']}\n";
-$message .= "電話番号: {$data['phone']}\n";
 $message .= "お問い合わせ内容:\n{$data['inquiryContent']}\n";
 $headers = "From: noreply@example.com";
 
