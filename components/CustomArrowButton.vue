540d46d68caa8a00375e4d520e2ad7ea2f331adf
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-07-09 10:59:50 +0900

    お問い合わせページの作成

--- components/CustomArrowButton.vue
+++ components/CustomArrowButton.vue
@@ -199,6 +199,35 @@ const router = useRouter();
 const isAnimating = ref(false);
 const animationStarted = ref(false);
 
+const handleTransitionEnd = (event: Event) => {
+    const link = event.currentTarget as HTMLElement;
+    if (event.target === link && animationStarted.value) {
+        link.removeEventListener('transitionend', handleTransitionEnd);
+        link.removeEventListener('transitionstart', handleTransitionStart);
+        isAnimating.value = false;
+        animationStarted.value = false;
+
+        // onClickプロパティが設定されている場合は実行する
+        if (props.onClick) {
+            props.onClick(event);
+        }
+
+        // toプロパティが設定されており、かつ'javascript:void(0);'でない場合はリンク遷移を行う
+        if (props.to && props.to !== 'javascript:void(0);') {
+            event.preventDefault();
+            const targetPage = props.to;
+            router.push(targetPage).then(() => {
+                useScrollToTarget();
+            });
+        }
+    }
+};
+
+// トランジション開始時の処理を定義する
+const handleTransitionStart = () => {
+    animationStarted.value = true;
+};
+
 const handleClick = (event: Event) => {
     const link = event.currentTarget as HTMLElement;
 
@@ -216,35 +245,6 @@ const handleClick = (event: Event) => {
         handleTransitionEnd(event);
         return;
     }
-
-    // トランジション開始時の処理を定義する
-    const handleTransitionStart = () => {
-        animationStarted.value = true;
-    };
-
-    // トランジション終了時の処理を定義する
-    const handleTransitionEnd = (event: Event) => {
-        if (event.target === link && animationStarted.value) {
-            link.removeEventListener('transitionend', handleTransitionEnd);
-            link.removeEventListener('transitionstart', handleTransitionStart);
-            isAnimating.value = false;
-            animationStarted.value = false;
-
-            // onClickプロパティが設定されている場合は実行する
-            if (props.onClick) {
-                props.onClick(event);
-            }
-
-            // toプロパティが設定されており、かつ'javascript:void(0);'でない場合はリンク遷移を行う
-            if (props.to && props.to !== 'javascript:void(0);') {
-                event.preventDefault();
-                const targetPage = props.to;
-                router.push(targetPage).then(() => {
-                    useScrollToTarget();
-                });
-            }
-        }
-    };
 
     // トランジション開始と終了イベントを監視する
     link.addEventListener('transitionstart', handleTransitionStart);
