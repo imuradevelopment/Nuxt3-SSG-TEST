05b2cf914c4605bc9d71127276608da1c63edc72
Yuta Miyaura <imuradevelopmentauth@gmail.com>
2024-07-30 19:24:09 +0900

    設定ファイルの更新

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,18 +7,13 @@ export default defineNuxtConfig({
             enabled: true,
         },
     },
-    ssr: true, // SSRを有効にする
-    // target: "static", // 静的ホスティングを対象とする
+    ssr: true,
     experimental: {
         payloadExtraction: false,
     },
     app: {
-        // pageTransition: { name: 'page', mode: 'out-in' },
-        // layoutTransition: { name: 'layout', mode: 'out-in' },
-        // NUXT_APP_BASE_URL
         baseURL: "/",
         // baseURL: '/Nuxt3-SSG-TEST',
-        // NUXT_APP_CDN_URL
         cdnURL: "/",
         // cdnURL: '/Nuxt3-SSG-TEST',
         head: {
@@ -40,7 +35,7 @@ export default defineNuxtConfig({
             },
         },
     },
-    modules: ["@element-plus/nuxt", "nuxt-microcms-module", "@nuxtjs/tailwindcss", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt", "@nuxt/image"],
+    modules: ["@element-plus/nuxt", "@nuxtjs/tailwindcss", "@pinia/nuxt", "@pinia-plugin-persistedstate/nuxt", "@nuxt/image"],
     vite: {
         css: {
             preprocessorOptions: {
@@ -50,40 +45,18 @@ export default defineNuxtConfig({
             },
         },
         build: {
-            rollupOptions: {
-                external: ["./swiper/element/bundle"],
-            },
         },
     },
     elementPlus: {
         importStyle: "scss",
         themes: ["dark"],
     },
-    microCMS: {
-        serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
-        apiKey: process.env.MICROCMS_API_KEY,
-        target: "all",
-    },
     css: ["~/assets/styles/css/reset.css", "~/assets/styles/css/style.css"],
     pinia: {
-        autoImports: [
-            // automatically imports `defineStore`
-            "defineStore", // import { defineStore } from 'pinia'
-            ["defineStore", "definePiniaStore"], // import { defineStore as definePiniaStore } from 'pinia'
-        ],
     },
     tailwindcss: {
         configPath: "~/tailwind.config.ts",
     },
     vue: {
-        compilerOptions: {
-            isCustomElement: (tag) => /^(swiper|swiper-slide|swiper-container)$/.test(tag),
-        },
     },
-    // scrollBehavior(to: any, from: any, savedPosition: any) {
-    //     console.log(to);
-    //     console.log(from);
-    //     console.log(savedPosition);
-    //     return { top: 0 };
-    // },
 });
